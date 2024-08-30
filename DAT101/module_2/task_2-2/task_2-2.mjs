9c1d8324e40d5291e67181be6628222c7e303162
heasp04 <heasp2004@gmail.com>
2024-08-30 18:51:36 +0200

    Stuff

--- DAT101/module_2/task_2-2/task_2-2.mjs
+++ DAT101/module_2/task_2-2/task_2-2.mjs
@@ -4,7 +4,13 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let originalAnswer;
+let modifiedAnswer;
+
+originalAnswer = 2 + 3 * 2 - 4 * 6;
+modifiedAnswer = 2 + 3 * (2 - 4) * 6;
+
+printOut(originalAnswer + ". " + modifiedAnswer);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
@@ -34,6 +40,10 @@ printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+if(5 > 3){
+
+}
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
