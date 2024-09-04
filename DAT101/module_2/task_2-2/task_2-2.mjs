4a41416c0e26639ba80e2593b47b650288925fd8
heasp04 <heasp2004@gmail.com>
2024-09-04 13:16:20 +0200

    Started a little on 2.2

--- DAT101/module_2/task_2-2/task_2-2.mjs
+++ DAT101/module_2/task_2-2/task_2-2.mjs
@@ -10,31 +10,44 @@ let modifiedAnswer;
 originalAnswer = 2 + 3 * 2 - 4 * 6;
 modifiedAnswer = 2 + 3 * (2 - 4) * 6;
 
-printOut(originalAnswer + ". " + modifiedAnswer);
+printOut("The original answer is " + originalAnswer + ". The modified answer is " + modifiedAnswer);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+let meters = 25;
+let centimeters = 34;
+let inchInMillimeters = 25.4;
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+let days = 3;
+let hours = 12;
+let minutes = 14;
+let seconds = 45;
+let totalMinutes;
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+//Gjenbruker samme navn som forige oppgave
+totalMinutes = 6322.52;
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+let totalDollars = 54;
+// 76 NOK = 8.6 USD
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+let sentence = "There is much between heaven and earth that we do not understand.";
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
@@ -53,12 +66,20 @@ printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+let r = Math.round(Math.random() * 360);
+
+printOut("This will now be a random number between 1 and 360: " + r);
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+let totalDays = 131;
+let weeks;
+days = 0;
+
+days = totalDays % 7;
+totalDays = totalDays - days;
+weeks = totalDays / 7;
+
+printOut("131 days is the same as " + weeks + " weeks and " + days + " days");
 printOut(newLine);
